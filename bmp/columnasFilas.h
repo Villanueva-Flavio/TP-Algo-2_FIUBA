077e37a8985debdc3a4c4827ab57eee5d1011f36
Villanueva-Flavio <fvillanueva@fi.uba.ar>
2022-12-03 19:38:25 -0300

    removed

--- bmp/columnasFilas.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLUMNASFILAS_H_
-#define COLUMNASFILAS_H_
-
-#include "EasyBMP.h"
-void columnasFilasImagen(int filas, int columnas , BMP imagen,int numroCapa);
-
-
-
-#endif /* COLUMNASFILAS_H_ */
